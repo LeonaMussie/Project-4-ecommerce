b932c410571bca5a6e9ea569bb6e6a9c0f315d9f
LeonaMussie <91215248+LeonaMussie@users.noreply.github.com>
2023-01-07 12:07:15 +0000

    First commit

---
+++ store/urls.py
@@ -0,0 +1,22 @@
+from turtle import update
+from django.urls import path
+from  . import views 
+
+
+urlpatterns = [
+  
+    path('home/', views.home, name="home"),
+
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+    
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    
+    
+] 
